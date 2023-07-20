8d76ca2bfbea66ffae09fce51a13140fdf58a581
Akin94 <akin.tutkan@gmail.com>
2023-07-20 23:32:48 +0300

    Added a tag in header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -28,7 +28,7 @@ const header = () => {
     }
   return (
     <div className="flex items-center gap-7 h-20 p-5">
-        <div className="bg-amber-600 rounded-lg p-3 text-2xl font-bold">Movie App</div>
+        <a href="/">  <div className="bg-amber-600 rounded-lg p-3 text-2xl font-bold"> Movie App</div> </a>
         <div className='flex flex-1 items-center gap-2 border p-3 rounded-lg'>
             <input onKeyDown={searchFunc} onChange={e => setKeyword(e.target.value)} placeholder="Arama Yapınız !!" className='outline-none flex-1 bg-transparent' type="text" />
             <BiSearch size={25}/>
